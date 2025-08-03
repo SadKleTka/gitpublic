a4adbf6dad870ed84b5cd77cc6aa55dbb358bd51
SadKleTka <kadinovaman321123@gmail.com>
2025-08-03 15:33:25 +0600

    NEW FUNCTION DELETE

--- GPTlessons.java
+++ GPTlessons.java
@@ -4,7 +4,7 @@ public class GPTlessons {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
-        HashMap<String, String> phoneBook = new HashMap<>();
+        TreeMap<String, String> phoneBook = new TreeMap<>();
         phoneBook.put("Uzan", "+996504513053");
         phoneBook.put("Aman", "+996707754532");
         phoneBook.put("Daniel", "+996505053597");
@@ -15,13 +15,15 @@ public class GPTlessons {
         if (stop1.equals("exit"))
             System.exit(0);
         while (true) {
-            System.out.println("You are in the MainMenu of PhoneBook. What do you want to do?");
-            System.out.println("If you want to add a new contact write: \"add\"");
-            System.out.println("If you want to find a contact write: \"find\"");
-            System.out.println("If you want to see a list of contacts write: \"list\"");
-            System.out.println("If you want to exit from a phoneBook, please write: \"exit\"");
+            System.out.println("\n📱 PHONEBOOK MENU");
+            System.out.println("--------------------------");
+            System.out.println("If you want to add a new contact write: \"add\" ➕");
+            System.out.println("If you want to find a contact write: \"find\" 🔍");
+            System.out.println("If you want to see a list of contacts write: \"list\" 📋");
+            System.out.println("If you want to remove some contacts write: \"remove\" ❌");
+            System.out.println("If you want to exit from a phoneBook, please write: \"exit\" \uD83D\uDED1");
             String mainmenu = scan.nextLine();
-            if (!mainmenu.equals("find") && !mainmenu.equals("add") && !mainmenu.equals("list") && !mainmenu.equals("exit")) {
+            if (!mainmenu.equals("find") && !mainmenu.equals("add") && !mainmenu.equals("list") && !mainmenu.equals("exit") && !mainmenu.equals("remove")) {
                 System.out.println("There are no commands like that. Please press \"ENTER\" to return to Main menu");
                 scan.nextLine();
             }
@@ -36,17 +38,23 @@ public class GPTlessons {
 
                 case "list":
                     ListOfContacts(phoneBook, scan);
+                    System.out.println("Press \"ENTER\" to return to the Main menu");
+                    scan.nextLine();
                     break;
 
                 case "exit":
                     System.exit(0);
+
+                case "remove":
+                    DeleteContacts(phoneBook, scan);
+                    break;
             }
         }
 
 
     }
 
-    public static void FindContact(HashMap<String, String> phoneBook, Scanner scan) {
+    public static void FindContact(TreeMap<String, String> phoneBook, Scanner scan) {
         while (true) {
             System.out.println("🔍 Contact Search");
             System.out.println("Enter \"back\" to back in the main menu or \"exit\" to leave from the app");
@@ -71,17 +79,20 @@ public class GPTlessons {
 
     }
 
-    public static void AddContact(HashMap<String, String> phoneBook, Scanner scan) {
+    public static void AddContact(TreeMap<String, String> phoneBook, Scanner scan) {
         while (true) {
             System.out.println("\n➕ Add New Contact");
             System.out.println("\nIf you want to back to the Main menu write \"back\"");
             System.out.print("\nWrite name: ");
             String name = scan.nextLine();
+            if (name.isEmpty()) {
+                System.out.println("\uD83D\uDED1❗This field cannot be empty‼\uFE0F");
+                continue;
+            }
             if (name.equals("back"))
                 return;
             if (phoneBook.containsKey(name)) {
-                System.out.println("You already have a contact with that name. Press \"ENTER\" to continue.");
-                scan.nextLine();
+                System.out.println("You already have a contact with that name\uD83D\uDEA8");
             } else {
                 System.out.print("\nWrite number: ");
                 String number = scan.nextLine();
@@ -104,16 +115,53 @@ public class GPTlessons {
         }
     }
 
-    public static void ListOfContacts(HashMap<String, String> phoneBook, Scanner scan) {
+    public static void ListOfContacts(TreeMap<String, String> phoneBook, Scanner scan) {
         System.out.println("\n📋 Contact List:");
         System.out.println("\n=================================================================================================");
         for (String name : phoneBook.keySet()) {
             System.out.println("Name: " + name + "\t Number: " + phoneBook.get(name));
         }
         System.out.println("=================================================================================================");
-        System.out.println("Press \"ENTER\" to return to the Main menu");
-        scan.nextLine();
 
+    }
+    public static void DeleteContacts(TreeMap<String, String> phoneBook, Scanner scan) {
+        while(true) {
+            System.out.println("\nRemove contact ❌");
+            System.out.println("\nWrite \"back\" if you want to return in the Main menu");
+            System.out.print("\n\nWrite name: ");
+            String name = scan.nextLine();
+            if(name.equals("back"))
+                break;
+            if (phoneBook.containsKey(name)) {
+                phoneBook.remove(name);
+                System.out.println("✅ Contact removed.");
+                System.out.println("Do you want to see new list of contacts? Print \"yes\" or \"no\"");
+                String yes = scan.nextLine();
+                if (yes.equals("yes")) {
+                    ListOfContacts(phoneBook, scan);
+                    System.out.println("Press \"ENTER\" to return to continue removing contacts, or write \"back\" to return in the Main menu");
+                    String back = scan.nextLine();
+                    if(back.equals("back"))
+                    break;
+                } else if (yes.equals("no")) {
+                    System.out.println("Press \"ENTER\" to return in the main menu");
+                    scan.nextLine();
+                    break;
+                } else {
+                    System.out.println("There are no commands like that.");
+                    System.out.println("Press \"ENTER\" to continue removing contacts, or enter \"back\" to return in the Main menu");
+                    String enter = scan.nextLine();
+                    if(enter.equals("back"))
+                        break;
+                }
+            } else {
+                System.out.println("❌ Contact not found.");
+                System.out.println("Press \"ENTER\" to continue removing contacts, or enter \"back\" to return in the Main menu");
+                String returnAfterError = scan.nextLine();
+                if(returnAfterError.equals("back"))
+                    break;
+            }
+        }
     }
 
 }
